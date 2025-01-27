9f7b991f59eea6df2389f665e77172f5aff342d9
Sharadhirai18 <sharadhirajbca24@rvu.edu.in>
2025-01-27 15:20:20 +0530

    Update test.py

--- lab1/test.py
+++ lab1/test.py
@@ -1,8 +1,5 @@
-
-x=int(input("give some number"))
+x=int(input("give some number:"))
 if x>0:
-  print("number is positive")
-elif x<0:
+ print("number is positive")
+ if x<0:
   print("number is negative")
-else:
-  print("number is zero")
